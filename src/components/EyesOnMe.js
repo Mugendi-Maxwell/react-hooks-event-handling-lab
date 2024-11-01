c89b75d50ac854addac73532d77d5ff98f816667
Maxi-arch <mxmu025@gmail.com>
2024-11-01 18:26:15 +0300

    1stcommit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function eyesOn() {
+        console.log("Good!")
+    }
+    function eyesOff(){
+        console.log("Hey! Eyes on me!");
+    }
+    return(
+        <div>
+            <button onFocus={eyesOn}  onBlur={eyesOff}>Eyes on me</button>
+        </div>
+    )
+}
+ 
+export default EyesOnMe;
